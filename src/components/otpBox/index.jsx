37288302ee7fe82b9599ba78f83980b083c8b923
Rayyan Akhter <rayyanakhter2003@gmail.com>
2023-10-22 17:56:43 +0530

    this is our first otp-input commit

---
+++ src/components/otpBox/index.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useRef } from "react";
+import "./style.css";
+
+export const Otp = () => {
+  const [otp, setOtp] = useState(["", "", "", "", "", ""]);
+  const inputRefs = [
+    useRef(),
+    useRef(),
+    useRef(),
+    useRef(),
+    useRef(),
+    useRef(),
+  ];
+
+  const handleInputChange = (e, index) => {
+    const value = e.target.value;
+    const updatedOtp = [...otp];
+    updatedOtp[index] = value;
+    setOtp(updatedOtp);
+
+    if (value === "") {
+      if (index > 0) {
+        inputRefs[index - 1].current.focus();
+      }
+    } else if (index < inputRefs.length - 1) {
+      inputRefs[index + 1].current.focus();
+    }
+  };
+
+  const handlePaste = (e) => {
+    e.preventDefault();
+    const pasteData = e.clipboardData.getData("text");
+    if (pasteData.length === otp.length) {
+      const updatedOtp = pasteData.split("");
+      setOtp(updatedOtp);
+      updatedOtp.forEach((value, index) => {
+        if (inputRefs[index]) {
+          inputRefs[index].current.value = value;
+        }
+      });
+    }
+  };
+
+  return (
+    <div className="main">
+      
+        <h2>
+        Enter Your Otp
+        </h2>
+        
+      <div className="inputFieldContainer" onPaste={handlePaste}>
+        {otp.map((data, index) => (
+          <input
+            type="text"
+            className="input"
+            maxLength={1}
+            value={data}
+            onChange={(e) => handleInputChange(e, index)}
+            onFocus={(e) => e.target.select()}
+            key={index}
+            ref={inputRefs[index]}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
